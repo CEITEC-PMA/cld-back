dba7b3d84a791364cff6d905c72274c132e03df6
Breno Gomes da Fonseca <brenogfonseca@gmail.com>
2023-10-11 12:47:54 -0300

    alterando rota para salvar documentos

--- config/multer.js
+++ config/multer.js
@@ -1,18 +1,19 @@
-const multer = require('multer');
-const fs = require('fs')
-
+const multer = require("multer");
+const fs = require("fs");
 
 //UPLOAD DE IMAGEM
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     let { cpf } = req.query;
-    cpf2 = cpf
+    cpf2 = cpf;
     cpf2 = cpf2.replace(".", "");
     cpf2 = cpf2.replace(".", "");
     cpf2 = cpf2.replace("-", "");
     var isvalidate = function (data) {
-
-      var dir =  "/var/www/html/backend/back-end-eleicao/tmp/doc__eleicao/candidatos" + "/" + cpf2
+      var dir =
+        "/var/www/html/backend/back-end-eleicao/tmp/doc__eleicao/candidatos" +
+        "/" +
+        cpf2;
       // var dir = "./tmp/doc__eleicao/candidatos" + "/" + cpf2
       if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir, { recursive: true });
@@ -22,15 +23,16 @@ const storage = multer.diskStorage({
       }
     };
 
-    callback(null, isvalidate())
-
+    callback(null, isvalidate());
   },
-  filename: (req, file, callback) => callback(null, file.fieldname + '-' + Date.now() + '.jpg'),
+  filename: (req, file, callback) =>
+    callback(null, file.fieldname + "-" + Date.now() + ".jpg"),
   fileFilter: (req, file, cb) => {
-
     // Procurando o formato do arquivo em um array com formatos aceitos
     // A função vai testar se algum dos formatos aceitos do ARRAY é igual ao formato do arquivo.
-    const isAccepted = ['image/jpg', 'image/jpg', 'image/jpeg'].find(formatoAceito => formatoAceito == file.mimetype);
+    const isAccepted = ["image/jpg", "image/jpg", "image/jpeg"].find(
+      (formatoAceito) => formatoAceito == file.mimetype
+    );
 
     // O formato do arquivo bateu com algum aceito?
     if (isAccepted) {
@@ -40,15 +42,14 @@ const storage = multer.diskStorage({
 
     // Se o arquivo não bateu com nenhum aceito, executamos o callback com o segundo valor false (validação falhouo)
     return cb(null, false);
-  }
+  },
 });
 
-const upload = multer({ storage })
+const upload = multer({ storage });
 
 //UPLOAD DE DOCUMENTOS
 
 const storageInscricao = multer.diskStorage({
-
   destination: (req, file, callback) => {
     const { cpf } = req.query;
 
@@ -58,7 +59,10 @@ const storageInscricao = multer.diskStorage({
     cpf2 = cpf2.replace("-", "");
 
     var isvalidate = function (data) {
-      var dir = "/var/www/html/pma-api/eleicao-educacao/tmp/doc__eleicao/candidatos" + "/" + cpf2
+      var dir =
+        "/var/www/html/backend/back-end-eleicao/tmp/doc__eleicao/candidatos" +
+        "/" +
+        cpf2;
       // var dir = "../tmp/doc__eleicao/candidatos" + "/" + cpf2
       if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir, { recursive: true });
@@ -68,11 +72,16 @@ const storageInscricao = multer.diskStorage({
       }
     };
 
-    callback(null, isvalidate())
-
+    callback(null, isvalidate());
   },
-  filename: (req, file, callback) => callback(null, file.fieldname + '-' + Date.now() + file.originalname.substring(file.originalname.indexOf("."))),
-
+  filename: (req, file, callback) =>
+    callback(
+      null,
+      file.fieldname +
+        "-" +
+        Date.now() +
+        file.originalname.substring(file.originalname.indexOf("."))
+    ),
 });
 
 const uploadCandidato = multer({
@@ -80,13 +89,13 @@ const uploadCandidato = multer({
     // if (file.mimetype !== 'application/pdf') {
     //   return callback(new Error('Extensão de arquivo não suportada'));
     // }
-    callback(null, true)
+    callback(null, true);
   },
   storage: storageInscricao,
-  limits: { fileSize: 5 * 1024 * 1024 }
-})
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
 
 module.exports = {
   upload,
   uploadCandidato,
-}
+};
