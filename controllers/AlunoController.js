12af65a79449abf9a447ff7b8849abcc4eb63eac
Breno Gomes da Fonseca <brenogfonseca@gmail.com>
2023-10-30 09:52:20 -0300

    mudando get aluno para zona em vez de inep

--- controllers/AlunoController.js
+++ controllers/AlunoController.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
-const Aluno = mongoose.model('Aluno');
-const Zona = mongoose.model('Zona');
+const mongoose = require("mongoose");
+const Aluno = mongoose.model("Aluno");
+const Zona = mongoose.model("Zona");
 
 class AlunoController {
-
   //ADM
   // get /admin
   async indexAdm(req, res, next) {
     try {
-      const zonaInep = await Zona.findOne({ _id: req.payload.id })
-      const alunos = await Aluno.find({ deletado: false, inep: zonaInep.inep }).collation({ locale: "en", strength: 1 }).sort({ nome: 1 });
+      const zonaInep = await Zona.findOne({ _id: req.payload.id });
+      const alunos = await Aluno.find({ deletado: false, inep: zonaInep.inep })
+        .collation({ locale: "en", strength: 1 })
+        .sort({ nome: 1 });
 
       return res.send({ alunos });
     } catch (e) {
@@ -17,9 +18,9 @@ class AlunoController {
     }
   }
 
-  async alunoInep(req, res, next) {
+  async alunoZona(req, res, next) {
     try {
-      const alunos = await Aluno.find({ inep: req.params.inep });
+      const alunos = await Aluno.find({ zona: req.payload.id });
 
       return res.send({ alunos });
     } catch (e) {
@@ -29,7 +30,7 @@ class AlunoController {
 
   async showSuperAdm(req, res, next) {
     try {
-      const zonaInep = await Zona.findOne({ _id: req.params.id })
+      const zonaInep = await Zona.findOne({ _id: req.params.id });
       const alunos = await Aluno.find({ inep: zonaInep.inep });
       return res.send({ alunos });
     } catch (e) {
@@ -40,13 +41,11 @@ class AlunoController {
   async inserirVotante(req, res, next) {
     //const zona = req.payload.id
     try {
-      const alunos = await Aluno.find(
-        { serie: /Sem/ }
-      );
-      alunos.map(item => {
-        item.votante = true
-        item.save()
-      })
+      const alunos = await Aluno.find({ serie: /Sem/ });
+      alunos.map((item) => {
+        item.votante = true;
+        item.save();
+      });
       return res.send({ alunos });
     } catch (e) {
       next(e);
@@ -55,27 +54,37 @@ class AlunoController {
 
   async showAll(req, res, next) {
     try {
-      var lista = []
-      await Zona.find({}, '_id inep').then(async response => {
-        await Promise.all(response.map(async item => {
-          const alunosTotais = await Aluno.count({ inep: item.inep })
-          const alunosVotantes = await Aluno.count({ inep: item.inep, votante: true })
-          lista.push({ unidade: item, qtd_alunos_total: alunosTotais, qtd_alunos_votantes: alunosVotantes, aluno: aluno })
-          return lista
-        }))
-      })
-      return res.send({ lista })
+      var lista = [];
+      await Zona.find({}, "_id inep").then(async (response) => {
+        await Promise.all(
+          response.map(async (item) => {
+            const alunosTotais = await Aluno.count({ inep: item.inep });
+            const alunosVotantes = await Aluno.count({
+              inep: item.inep,
+              votante: true,
+            });
+            lista.push({
+              unidade: item,
+              qtd_alunos_total: alunosTotais,
+              qtd_alunos_votantes: alunosVotantes,
+              aluno: aluno,
+            });
+            return lista;
+          })
+        );
+      });
+      return res.send({ lista });
     } catch (e) {
-      next(e)
+      next(e);
     }
   }
 
   async showAlunoAll(req, res, next) {
     try {
-      const aluno = await Aluno.find({}, 'inep nome turma responsavel mae pai')
-      return res.send({ aluno })
+      const aluno = await Aluno.find({}, "inep nome turma responsavel mae pai");
+      return res.send({ aluno });
     } catch (e) {
-      next(e)
+      next(e);
     }
   }
 
@@ -95,81 +104,106 @@ class AlunoController {
   //   }
   // }
 
-
   // // get /adm/:id
   async showAdm(req, res, next) {
     try {
-      const aluno = await Aluno
-        .findOne({ idescola: req.payload.id, _id: req.params.id })
-      return res.send({ aluno })
+      const aluno = await Aluno.findOne({
+        idescola: req.payload.id,
+        _id: req.params.id,
+      });
+      return res.send({ aluno });
     } catch (e) {
-      next(e)
+      next(e);
     }
   }
 
   async remove(req, res, next) {
     try {
-      const { ids, deletado } = req.body
-      ids.map(async item => {
-        const aluno = await Aluno.findOne(item)
-        aluno.deletado = deletado
-        await aluno.save()
-      })
-      return res.send({ deletado: false })
+      const { ids, deletado } = req.body;
+      ids.map(async (item) => {
+        const aluno = await Aluno.findOne(item);
+        aluno.deletado = deletado;
+        await aluno.save();
+      });
+      return res.send({ deletado: false });
     } catch (e) {
-      console.log(e)
-      next(e)
+      console.log(e);
+      next(e);
     }
   }
 
   async addAluno(req, res, next) {
     try {
-      const { alunos } = req.body
-      alunos.map(async item => {
-        const { nome, inep, mae, pai, responsavel, serie, turma, votante } = item
-        const aluno = new Aluno({ nome, inep, mae, pai, responsavel, serie, turma, votante });
-        aluno.save()
-      })
-      return res.send({ message: "alunos adicionados" })
+      const { alunos } = req.body;
+      await Promise.all(
+        alunos.map(async (item) => {
+          const {
+            nome,
+            inep,
+            mae,
+            pai,
+            responsavel,
+            serie,
+            turma,
+            votante,
+            zona,
+          } = item;
+          const aluno = new Aluno({
+            nome,
+            inep,
+            mae,
+            pai,
+            responsavel,
+            serie,
+            turma,
+            votante,
+            zona,
+          });
+          aluno.save();
+        })
+      );
+      return res.send({ message: "alunos adicionados" });
     } catch (e) {
-      console.log(e)
-      next(e)
+      console.log(e);
+      next(e);
     }
   }
 
   async removeNome(req, res, next) {
     try {
-      const { nomes } = req.body
-      const alunosNaoDeletados = []
-      await Promise.all(nomes.map(async (item) => {
-        const aluno = await Aluno.findOne({ nome: item.nome }).collation({ locale: "en", strength: 1 })
-        if (aluno) {
-          aluno.deletado = true
-          await aluno.save()
-        }
-        else {
-          alunosNaoDeletados.push(item)
-        }
-      }));
-      return res.send({ alunos: alunosNaoDeletados })
+      const { nomes } = req.body;
+      const alunosNaoDeletados = [];
+      await Promise.all(
+        nomes.map(async (item) => {
+          const aluno = await Aluno.findOne({ nome: item.nome }).collation({
+            locale: "en",
+            strength: 1,
+          });
+          if (aluno) {
+            aluno.deletado = true;
+            await aluno.save();
+          } else {
+            alunosNaoDeletados.push(item);
+          }
+        })
+      );
+      return res.send({ alunos: alunosNaoDeletados });
     } catch (e) {
-      console.log(e)
-      next(e)
+      console.log(e);
+      next(e);
     }
   }
 
   //GET /search/:search/pedidos
   async searchAlunos(req, res, next) {
     const { offset, limit } = req.query;
-    const zona = req.payload.id
+    const zona = req.payload.id;
     try {
-      const search = new RegExp(req.params.search, 'i');
-      const alunos = await Aluno.paginate(
-        {
-          idescola: zona,
-          nome: { $regex: search }
-        }
-      );
+      const search = new RegExp(req.params.search, "i");
+      const alunos = await Aluno.paginate({
+        idescola: zona,
+        nome: { $regex: search },
+      });
       return res.send({ alunos });
     } catch (e) {
       next(e);
--- routes/api/v1/aluno.js
+++ routes/api/v1/aluno.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
 
 const AlunoController = require("../../../controllers/AlunoController");
-const { ZonaValidation } = require("../../../controllers/validacoes/zonaValidation");
-const { AlunoValidation } = require("../../../controllers/validacoes/alunoValidation");
-const { AdmValidation } = require("../../../controllers/validacoes/admValidation");
-const upload = require('../../../config/multer');
+const {
+  ZonaValidation,
+} = require("../../../controllers/validacoes/zonaValidation");
+const {
+  AlunoValidation,
+} = require("../../../controllers/validacoes/alunoValidation");
+const {
+  AdmValidation,
+} = require("../../../controllers/validacoes/admValidation");
+const upload = require("../../../config/multer");
 
 const validate = require("express-validation");
 const auth = require("../../auth");
@@ -12,14 +18,29 @@ const auth = require("../../auth");
 const alunoController = new AlunoController();
 
 // ADM
-router.get('/', auth.required, ZonaValidation.adm, alunoController.indexAdm);
-router.get('/alunoInep/:inep', auth.required, AdmValidation.adm, alunoController.alunoInep);
-router.get('/lista', alunoController.showAll);
-router.get('/alunoAll', alunoController.showAlunoAll);
-router.get('/lista/alunos/:id', auth.required, AdmValidation.adm, alunoController.showSuperAdm);
-router.get('/:id', auth.required, ZonaValidation.adm, alunoController.showAdm);
-router.get('/inserir/votante', alunoController.inserirVotante);
-router.get("/search/:search", auth.required, ZonaValidation.adm, alunoController.searchAlunos);
+router.get("/", auth.required, ZonaValidation.adm, alunoController.indexAdm);
+router.get(
+  "/alunoZona/",
+  auth.required,
+  AdmValidation.adm,
+  alunoController.alunoZona
+);
+router.get("/lista", alunoController.showAll);
+router.get("/alunoAll", alunoController.showAlunoAll);
+router.get(
+  "/lista/alunos/:id",
+  auth.required,
+  AdmValidation.adm,
+  alunoController.showSuperAdm
+);
+router.get("/:id", auth.required, ZonaValidation.adm, alunoController.showAdm);
+router.get("/inserir/votante", alunoController.inserirVotante);
+router.get(
+  "/search/:search",
+  auth.required,
+  ZonaValidation.adm,
+  alunoController.searchAlunos
+);
 router.post("/", alunoController.addAluno); //testado
 // router.post("/", auth.required, AdmValidation.adm, validate(ZonaValidation.store), alunoController.store); //testado
 // router.put("/:id", auth.required, ZonaValidation.adm, validate(AlunoValidation.update), alunoController.update); //testado
@@ -29,7 +50,5 @@ router.delete("/removerPorNome", alunoController.removeNome); //testado
 // // POPULAÇÃO
 // router.get("/", validate(ZonaValidation.adm), alunoController.showAll); //testado
 // router.get("/:id", validate(ZonaValidation.adm), alunoController.showAdm); //testado
-
-
 
 module.exports = router;
